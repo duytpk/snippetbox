fc6231bbcdad70750720078f28b4f97755e71aac
vaodayvoianh_2003@yahoo.com <vaodayvoianh_2003@yahoo.com>
2021-03-16 11:00:39 +0700

    Ch 2.4

--- main.go
+++ main.go
@@ -6,14 +6,28 @@ import (
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+
 	w.Write([]byte("Hello from Snippetbox"))
 }
 
+func showSnippet(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display a specific snippet..."))
+}
+
+func createSnippet(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Create a new snippet..."))
+}
+
 func main() {
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", home)
+	http.HandleFunc("/", home)
+	http.HandleFunc("/snippet", showSnippet)
+	http.HandleFunc("/snippet/create", createSnippet)
 
 	log.Println("Starting server on :4000")
-	err := http.ListenAndServe(":4000", mux)
+	err := http.ListenAndServe(":4000", nil)
 	log.Fatal(err)
 }
