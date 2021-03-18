36cc8b478384728637b550ddd594eadcd12cb366
vaodayvoianh_2003@yahoo.com <vaodayvoianh_2003@yahoo.com>
2021-03-18 17:33:10 +0700

    Ch 4.5

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -46,5 +46,16 @@ func (app *application) createSnippet(w http.ResponseWriter, r *http.Request) {
 		app.clientError(w, http.StatusMethodNotAllowed)
 		return
 	}
-	w.Write([]byte("Create a new snippet..."))
+
+	title := "O snail"
+	content := "O snail\nClimb Mount Fuji,\nBut slowly, slowly!\n\n– Kobayashi"
+	expires := "7"
+
+	id, err := app.snippets.Insert(title, content, expires)
+	if err != nil {
+		app.serverError(w, err)
+		return
+	}
+
+	http.Redirect(w, r, fmt.Sprintf("/snippet?id=%d", id), http.StatusSeeOther)
 }
