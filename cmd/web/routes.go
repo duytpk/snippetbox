061cd4a5f7e256578345859ad4c3d667af889be4
vaodayvoianh_2003@yahoo.com <vaodayvoianh_2003@yahoo.com>
2021-03-18 11:28:56 +0700

    Ch 4.1

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -7,7 +7,9 @@ func (app *application) routes() *http.ServeMux {
 	mux.HandleFunc("/", app.home)
 	mux.HandleFunc("/snippet", app.showSnippet)
 	mux.HandleFunc("/snippet/create", app.createSnippet)
+
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
 	return mux
 }
