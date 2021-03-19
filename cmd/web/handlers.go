1fc3db0a4eb790729abbffff929a22ceb91f1954
vaodayvoianh_2003@yahoo.com <vaodayvoianh_2003@yahoo.com>
2021-03-19 16:30:24 +0700

    Ch 5.2

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -19,11 +19,10 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 		app.serverError(w, err)
 		return
 	}
-	for _, snippet := range s {
-		fmt.Fprintf(w, "%v\n", snippet)
-	}
 
-	/* files := []string{
+	data := &templateData{Snippets: s}
+
+	files := []string{
 		"./ui/html/home.page.tmpl",
 		"./ui/html/base.layout.tmpl",
 		"./ui/html/footer.partial.tmpl",
@@ -35,10 +34,10 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	err = ts.Execute(w, nil)
+	err = ts.Execute(w, data)
 	if err != nil {
 		app.serverError(w, err)
-	} */
+	}
 }
 
 func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
