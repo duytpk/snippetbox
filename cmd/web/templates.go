676cb38a8cacb3d0914a14e44df0e997e6f2b13f
vaodayvoianh_2003@yahoo.com <vaodayvoianh_2003@yahoo.com>
2021-03-23 18:01:55 +0700

    Ch 5.6

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -4,12 +4,21 @@ import (
 	"duytpk/snippetbox/pkg/models"
 	"path/filepath"
 	"text/template"
+	"time"
 )
 
 type templateData struct {
 	CurrentYear int
 	Snippet     *models.Snippet
 	Snippets    []*models.Snippet
+}
+
+func humanDate(t time.Time) string {
+	return t.Format("02 Jan 2006 at 15:04")
+}
+
+var functions = template.FuncMap{
+	"humanDate": humanDate,
 }
 
 func newTemplateCache(dir string) (map[string]*template.Template, error) {
@@ -21,19 +30,25 @@ func newTemplateCache(dir string) (map[string]*template.Template, error) {
 
 	for _, page := range pages {
 		name := filepath.Base(page)
-		ts, err := template.ParseFiles(page)
+
+		ts, err := template.New(name).Funcs(functions).ParseFiles(page)
 		if err != nil {
 			return nil, err
 		}
+
 		ts, err = ts.ParseGlob(filepath.Join(dir, "*.layout.tmpl"))
 		if err != nil {
 			return nil, err
 		}
+
 		ts, err = ts.ParseGlob(filepath.Join(dir, "*.partial.tmpl"))
 		if err != nil {
 			return nil, err
 		}
+
 		cache[name] = ts
 	}
+
 	return cache, nil
+
 }
