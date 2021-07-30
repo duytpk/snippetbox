6e03722893459f958a521acd29fb2f12e0f2f6d4
DuyTPK <vaodayvoianh_2003@yahoo.com>
2021-07-30 22:09:03 +0700

    Ch 11.5

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -141,5 +141,8 @@ func (app *application) loginUser(w http.ResponseWriter, r *http.Request) {
 }
 
 func (app *application) logoutUser(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Logout the user...")
+	app.session.Remove(r, "userID")
+
+	app.session.Put(r, "flash", "You've been logged out successfully!")
+	http.Redirect(w, r, "/", 303)
 }
